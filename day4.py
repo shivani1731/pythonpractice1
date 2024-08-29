11d0f0032f9867f42e0e90882cffe18a855ddbd6
shivani anil choudhary <shivani.mahi1711@gmail.com>
2024-08-29 18:09:42 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,50 @@
+#first="abcd"
+#last="xyz"
+#fullname=first+last
+#print(fullname)
+
+
+#first="abcd"
+#last="xyz"
+#fullname=first+" "+last
+#print(fullname)
+
+#first="abcd"
+#last="xyz"
+#age=12
+#print( "hello my name is"  + first  + "!" )
+
+
+#split
+
+#x="20-08-2024"
+#print(type(x.split("-")))
+
+
+#x="20-06-2024"
+#y=x.replace("/","-")
+#print(y)
+#print(y.split("-"))
+
+
+
+
+    
+#string formating
+#first="abcd"
+#last="xyz"
+#age=12
+#print(f"hello my name is {first} {last} and i am {age} years")
+
+
+
+
+
+#strip
+x="   abcd  xyz   "
+print(x)
+print(len(x.strip()))
+
+
+
+
