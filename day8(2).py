61619adfe55448beba9217703da8c647b8f70b01
shivani anil choudhary <shivani.mahi1711@gmail.com>
2024-09-10 00:38:36 +0530

    Add files via upload

---
+++ day8(2).py
@@ -0,0 +1,24 @@
+#a=1
+#while a<11:
+#    print("hello")
+#    a=a+10
+#print("xyz")
+
+
+#a=1
+#while a<101:
+#    print("hyy")
+#    a=a+1
+
+
+#a=1
+#while a<101:
+#    print(a)
+#    a=a+1
+
+
+#a=1
+#while a<101:
+#    if a%2==0:
+#        print(a)
+#        a+=1
