4d2a8c3019361a48ecf053235985cc51d6b8ae45
shivani anil choudhary <shivani.mahi1711@gmail.com>
2024-09-27 14:25:10 +0530

    Add files via upload

---
+++ listday13.py
@@ -0,0 +1,38 @@
+#remove method
+#x=[10,20,30,35,40]
+#x.remove(35)
+#print(x)
+
+#index wise remove pop with index
+#x=[10,20,30,35,40,45]
+#x.pop(3)
+#print(x)
+
+#pop without index
+#x=[10,20,30,40,45,50,55]
+#x.pop()
+#print(x)
+
+#delet method
+#x=[10,20,30,40]
+#del x [2]
+#print(x)
+
+#clear method
+#x=[10,20,30,40,50,60]
+#x.clear()
+#print(x)
+
+#copy method
+#x=[10,20,30,40,50]
+#y=x.copy()
+#y[2]=100
+#print(y)
+#print(x)
+
+#list comprehesion
+#x=[1,2,3,4,5,6,7,8,9,10,2,4,6,8,5,3]
+#even=[i for i in x if 1%2 == 0]
+#odd=[i for i in x if 1%2! == 0]
+#print(even)
+#print(odd)
