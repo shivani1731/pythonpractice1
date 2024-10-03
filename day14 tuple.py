74bc87aed9ef6cc29fc42b455d1be948262547cb
shivani anil choudhary <shivani.mahi1711@gmail.com>
2024-10-03 15:06:56 +0530

    Add files via upload

---
+++ day14 tuple.py
@@ -0,0 +1,39 @@
+#tuple.............
+#x=(10,20,30,40)
+#print(type(x))
+
+
+#x=(10,20,30,40)
+#print(x[0])
+
+#x=(10,20,30,40)
+#print(x[0:2])
+
+
+#....unpack tuple....
+#age=20,30,40
+#ram,shyam,rahul=age
+#print(ram)
+
+
+#x=(10,20,30,40,10)
+#print(x.count(10))
+
+#x=(10,20,30,40,50)
+#print(x.index(20))
+
+#how to convert tuple in list................
+#x=(10,20,30,40)
+#y=list(x)
+#y[10]=100
+#y.remove(30)
+#x=tuple(y)
+#print(x)
+
+#x=(1,2,3,4,5,6,8,9)
+#for i in x:
+#    if 1%2==0:
+#        print(i)
+       
+       
+       
