a64e49d6afdca23ab8f714ecd81391f3210d5e85
shivani anil choudhary <shivani.mahi1711@gmail.com>
2024-10-04 12:32:48 +0530

    Add files via upload

---
+++ day16 practice.py
@@ -0,0 +1,58 @@
+#python program to find the second largerst element in a list?
+#x=[1,2,3,4,5,6,8,9,1,3]
+#x.sort()
+#print(x[-2])
+
+#the sum of all element in a list without using the() function?
+#x=[1,2,3,5,6,8,9,0,2]
+#sum=0
+#for i in x:
+#    sum=sum+i
+#print("f"total sum of element.{sum}")
+
+
+#check if a list is empty or not in python?
+#x=[1]
+#if len (x)==0:
+#    print("list is empty",x)
+
+#reverse a list without using the reverse () function?
+#x=[1,2,3,4,5,6,7]
+#print(x[5:-1])
+
+#python program to remove duplicate form a list?
+#x=[1,21,3,4,6,7,8,9,5,8]
+#y=set(x)
+#x=list(y)
+#print(x)
+
+#python program to merger two shorted list into a single shorted list?
+#x=[1,2,3,4,5,6]
+#y=[1,2,3,4,5,6]
+#x.extend(y)
+#x.sort()
+#print(x)
+
+#find the common  element in two list?
+#x=[1,2,3,4,5,6]
+#y=[2,4,6,7,8,9]
+#print("below element are common boyh the list")
+#for i in x:
+#    for j in y:
+#        if i==j:
+#            print(i)
+
+
+#remove all occurrences of a specific element from a list?
+#x=[1,2,3,4,5,6,7,8,9]
+#for i in range (len(x)):
+#    x[i]=x[i]*x[i]
+#print(x)
+
+#differnce between two list element in one list but not in other
+#x=[10,20,30,40,50,60]
+#y=[55,22,88,33,55,88]  
+#z=[]
+#for i,j in zip(x,y):
+#    z.append(i-j)
+#print(z)  
