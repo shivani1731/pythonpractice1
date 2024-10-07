7dd504c662a3da872cc5b3d65836e6d26e0ce45a
shivani anil choudhary <shivani.mahi1711@gmail.com>
2024-10-07 13:03:18 +0530

    Add files via upload

---
+++ day 18.py
@@ -0,0 +1,15 @@
+#sales={'salesperson':['A','B','C','D','E'],
+#       'Amount':[1800,600,1200,1100,900],
+#       'Product':['earpods','phonecover','earpods','earpods','phonecover']}
+#count=0
+#total_salesperson=len(sales['salesperson'])
+#print('Count of salesperson are',total_salesperson)
+
+#total_sale=sum(sales['Amount'])
+#print('Total sale is',total_sale) 
+
+#unique_product=set(sales['Product'])
+#print('The unique product are',unique_product)
+
+
+
